e078bf9c4b29667a7061e7941894a493ce2c2ca9
xingzhou <xingzhou@xingzhoudeMacBook-Pro.local>
2019-03-20 16:45:39 -0700

    Submiting team assignment

---
+++ DicePairTest.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class DicePairTest {
+
+	public static void main(String[] args)
+	{
+		
+
+		DicePair dice = new DicePair();
+		
+		System.out.println("The sum: " + dice.roll());
+	}
+}
