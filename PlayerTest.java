35828406a022f951e95b9de3bb1aa0a31d95aed0
Lfsanchez2 <47400025+Lfsanchez2@users.noreply.github.com>
2019-03-20 20:01:36 -0700

    Update PlayerTest.java

--- PlayerTest.java
+++ PlayerTest.java
@@ -1,23 +1,29 @@
+import java.util.Random;
 public class PlayerTest{
-  public static main(String[] args){
+  public static void main(String[] args){
+    Random gen = new Random();
     Player first = new Player("Anthony");
     Player second = new Player ("Meagen");
-    first.setPoints(6);
-    second.setPoints(3);
-    if (first.getPoints()>second.getPoints()){
-      System.out.println(">> "+first.getName()+" wins!");
-			first.won();
-			second.lost();
+    for (int i=0; i<5; i++){
+      first.setPoints(gen.nextInt(7)+1);
+      second.setPoints(gen.nextInt(7)+1);
+      if (first.getPoints()>second.getPoints()){
+        System.out.println(">> "+first.getName()+" wins!");
+  			first.won();
+  			second.lost();
+      }
+      else if (second.getPoints()>first.getPoints()){
+        System.out.println(">> "+second.getName()+" wins!");
+  			first.lost();
+  			second.won();
+      }
+      else{
+  	System.out.println(">> It's a tie!"); 
+  	first.won(); 
+  	second.won();
+  	}	
+      System.out.println(first);
+      System.out.println(second);
     }
-    else if (second.getPoints()>first.getPoints()){
-      System.out.println(">> "+second.getName()+" wins!");
-			first.lost();
-			second.won();
-    }
-    else {
-			System.out.println(">> It's a tie!"); 
-			first.won(); 
-			second.won();
-		}	
   }
 }
