6568ed6d0c4e1aabd12377ea3c2842a4272ef696
vishusikka <47587906+vishusikka@users.noreply.github.com>
2019-03-20 21:04:21 -0700

    Add files via upload

--- Coin.java
+++ Coin.java
@@ -16,8 +16,8 @@ public class Coin {
       sideUp = "Tails";
     }
   }
-  //Try renaming toss to flip(), that's what it's called in DiceGame.java
-  public void toss() {
+  
+  public void flip() {
     
     int randomNumber = generator.nextInt(2);
     
@@ -28,9 +28,11 @@ public class Coin {
     }
   }
   
-  //So instead of this method, we need to make one called isHeads().  I think the best way would be to write it out like this:
-  // public boolean isHeads(), and then have it return true if sideUp == "Heads" and false if sideUp == "Tails"
-  public String getSideUp() {
-    return sideUp;
+  public boolean isHeads() {
+    if(sideUp == "Heads") {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
